ffc53c20100511e69a371b85617eba4b05702e7d
fadiHB <eng.fadi.hb@gmail.com>
2022-10-29 09:10:50 +0200

    update categories

--- src/components/storefront/Categories.js
+++ src/components/storefront/Categories.js
@@ -13,6 +13,7 @@ import FastfoodIcon from '@mui/icons-material/Fastfood';
 import DevicesOtherIcon from '@mui/icons-material/DevicesOther';
 import CheckroomIcon from '@mui/icons-material/Checkroom';
 import DiamondIcon from '@mui/icons-material/Diamond';
+import CategoryIcon from '@mui/icons-material/Category';
 
 
 import { getCategories } from "../../store/actions/actions"
@@ -27,12 +28,30 @@ function Categories(props) {
         const icon = {
             "electronics": <DevicesOtherIcon />,
             "Food": <FastfoodIcon />,
-            "Clothing": <CheckroomIcon />,
             "jewelery": <DiamondIcon />,
+            "Clothing": <CheckroomIcon />,
             "men's clothing": <CheckroomIcon />,
             "women's clothing": <CheckroomIcon />
         }
-        return icon[categoryName];
+        return icon[categoryName] ? icon[categoryName] : <CategoryIcon />;
+
+        // switch (categoryName) {
+        //     case "electronics":
+        //         return <DevicesOtherIcon />
+        //     case "Food":
+        //         return <FastfoodIcon />
+        //     case "Clothing":
+        //     case "men's clothing":
+        //     case "women's clothing":
+        //         return <CheckroomIcon />
+        //     case "jewelery":
+        //         return <DiamondIcon />
+        //     default:
+        //         return <CategoryIcon />
+        // }
+
+
+        // return icon[categoryName];
     }
 
     React.useEffect(() => {
@@ -51,17 +70,20 @@ function Categories(props) {
                     }}
                 >
 
-                    {props.categories.map(category =>
+                    {props.categories.length < 1 ?
+                        <span>loading categories...</span>
+                        :
+                        props.categories.map(category =>
 
-                        <BottomNavigationAction
-                            key={category}
-                            label={category}
-                            icon={pickIcon(category)}
-                            onClick={() => props.selectedCategory(category)}
-                        />
+                            <BottomNavigationAction
+                                key={category}
+                                label={category}
+                                icon={pickIcon(category)}
+                                onClick={() => props.selectedCategory(category)}
+                            />
 
 
-                    )}
+                        )}
                 </BottomNavigation>
             </Box>
         </div>
--- src/store/reducers/categories.js
+++ src/store/reducers/categories.js
@@ -5,13 +5,7 @@ import { ACTIVE_CATEGORY, INIT_CATEGORIES } from "../actions/types";
 
 // InitialState
 let initialState = {
-    categoryList: [
-        // { _id: 1, name: 'electronics', displayName: 'Elecronics' },
-        // { _id: 2, name: 'jewelery', displayName: 'Jewelery' },
-        // { _id: 3, name: "men's clothing", displayName: "men's clothing" },
-        // { _id: 4, name: "women's clothing", displayName: "women's clothing" },
-        // { _id: 3, name: 'clothing', displayName: 'Clothing' },
-    ],
+    categoryList: [],
     activeCategory: ''
 };
 
